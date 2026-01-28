f6abd1f5e4e9e7f854d17473ae531d49f7dec0b5
Girisha <girishamunnangi@gmail.com>
2026-01-28 21:30:28 +0530

    special variables

---
+++ special-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "all args passed to the script $@"
+echo "number of args passed to the script $#"
+echo "script name $0"
+echo "present working directory $PWD"
+echo "who is the user $USER"
+echo "home directory of the user $HOME"
+echo "pid of the script $$"
+echo "pid of the background process $!"
