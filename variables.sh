cf31d395803beda2beb92b1b8c24f99cc070f937
Girisha <girishamunnangi@gmail.com>
2026-01-28 19:23:24 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=suresh
+PERSON2=ramesh
+
+echo "$PERSON1::hi $PERSON2 how r u?"
+echo "$PERSON2 :: hi $PERSON1 i am good , how about you?"
+echo "$PERSON1::i am also good.what are you learning $PERSON2?"
+echo "$PERSON2:: i am learning shell scripting"
