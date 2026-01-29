a23563c0031606a0451b1ef2cae8e27bf211ddf7
Girisha <girishamunnangi@gmail.com>
2026-01-29 21:58:06 +0530

    logs file

--- logs.sh
+++ logs.sh
@@ -2,7 +2,6 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
-mkdir -p "$LOG_FOLDER"
 
 if [ $USERID -ne 0 ]; then
     echo "run with root privilages" | tee -a $LOGS_FILE
