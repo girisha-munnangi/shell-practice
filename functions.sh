213d778ff137b9037360a9090a52b6564137d66f
Girisha <girishamunnangi@gmail.com>
2026-01-29 19:08:08 +0530

    if condition

---
+++ functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "run with root privilages"
+    exit 1
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ];then
+        echo "$2 is fail"
+        exit 1
+    else 
+        echo "$2 is success
+    fi
+}
+    
+dnf install nginx -y
+VALIDATE $? "installing nginx software"
+
+dnf install mysql -y
+VALIDATE $? "installing mysql software"
+
+dnf install nodejs -y
+VALIDATE $? "installing node js"
+
+
