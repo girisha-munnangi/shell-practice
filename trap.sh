103cfd4bf6006389237c598be416ca2c91679ca1
Girisha <girishamunnangi@gmail.com>
2026-01-30 18:32:03 +0530

    set command

--- trap.sh
+++ trap.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
-trap 'echo "there is an error in $LINENO,command: $BASH_COMMAND"' ERR
+
+set -e
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
+
 USERID=$(id -u)
-if [ $U
-SERID -ne 0 ]; then
-    echo "run with root privilages"
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
-fi
-    echo "installing nginx software"
-dnf install nginx -y
-if [ $? -ne 0 ]; then
-    echo "installation failed"
-    exit 1
-else
-    echo "installation success"
-fi
-echo "installing mysql software"
-dnf install mysql -y
-if [ $? -ne 0 ];then
-    echo"installation is fail"
-    exit 1
-else 
-    echo "installation is success"
-fi
-    echo "installing node js"
-dnf install nodejs -y
-if [ $? -ne 0 ]; then
-    echo "installation is fail"
-    exit 1
-else
-    echo "installation is success"
 fi
 
+mkdir -p $LOGS_FOLDER
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>>$LOGS_FILE
+    else
+        echo -e "$package already installed ... $Y SKIPPING $N"
+    fi
+done
