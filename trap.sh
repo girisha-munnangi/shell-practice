01d6dd7e383159fb611c384d2a7c342605d52c84
Girisha <girishamunnangi@gmail.com>
2026-01-30 19:25:38 +0530

    set command

--- trap.sh
+++ trap.sh
@@ -22,7 +22,7 @@ do
     dnf list installed $package &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
         echo "$package not installed, installing now"
-        dnf install $package -y &>> $LOGS_FILE
+        dnf install "$package" -y &>> $LOGS_FILE
     else
         echo -e "$package already installed ... $Y SKIPPING $N"
     fi
