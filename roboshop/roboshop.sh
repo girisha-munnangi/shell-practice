5f1752e9a6fc69d0843b347c1a6b4e829e89e196
Girisha <girishamunnangi@gmail.com>
2026-01-31 22:17:22 +0530

    roboshop shell

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-
-SG_ID="sg-014ceb176cdbf1d8a" # replace with your ID
+SG_ID="sg-014ceb176cdbf1d8a"
 AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z07285551FQZIZ26CYO03"
 DOMAIN_NAME="girisha.online"
-
 for instance in $@
 do
-    INSTANCE_ID=$( aws ec2 run-instances \
+INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
-    --output text )
-
+    --output text 
+    )
     if [ $instance == "frontend" ]; then
         IP=$(
             aws ec2 describe-instances \
@@ -22,28 +20,26 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
-        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
-    else
-        IP=$(
-            aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
-            --query 'Reservations[].Instances[].PrivateIpAddress' \
-            --output text
-        )
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
-    fi
-
-    echo "IP Address: $IP"
-
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Updating record",
-        "Changes": [
+        RECORD_NAME="$DOMAIN_NAME"
+        else
+            IP=$(
+                aws ec2 describe-instances \
+                --instance-ids $INSTANCE_ID \
+                --query 'Reservations[].Instances[].PrivateIpAddress' \
+                --output text
+            )
+            RECORD_NAME="$instance.$DOMAIN_NAME"
+        fi
+        echo "IP Address: $IP"
+        aws route53 change-resource-record-sets \
+         --hosted-zone-id $ZONE_ID \
+         --change-batch '
+         {
+            "Comment": "Updating record",
+            "Changes": [
             {
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
+                 "Action": "UPSERT",
+                "ResourceRecordSet": {
                 "Name": "'$RECORD_NAME'",
                 "Type": "A",
                 "TTL": 1,
@@ -57,7 +53,5 @@ do
         ]
     }
     '
-
-    echo "rec ord updated for $instance"
-
-done
+    echo "record updated for $instance"
+    done
