c65dd742f438c0749bbaa237de4c761bc5dbcbad
Girisha <girishamunnangi@gmail.com>
2026-02-01 23:18:46 +0530

    roboshop project

---
+++ roboshop/cart.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongod.girisha.online
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOGS_FILE
+dnf module enable nodejs:20 -y
+VALIDATE $? "disabling and enabling nodejs versions"
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
+
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "creating system user"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+
+fi
+mkdir /app &>>$LOGS_FILE
+VALIDATE $? "CREATING APP FOLDER"
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOGS_FILE
+VALIDATE $? "getting cart service code"
+cd /app 
+VALIDATE $? "Changing to app directory"
+unzip /tmp/cart.zip
+VALIDATE $? "unzipping the cart zip code"
+cd /app 
+npm install &>>$LOGS_FILE
+VALIDATE $? "installing npm"
+
+cp $CRIPT_DIR/cart.service /etc/system/systemd/cart.service
+VALIDATE $? "copying cart service file"
+systemctl daemon-reload &>>$LOGS_FILE
+VALIDATE $? "deamon reload"
+systemctl enable cart &>>$LOGS_FILE
+systemctl start cart &>>$LOGS_FILE
+VALIDATE $? "enable and starting the cart service "
