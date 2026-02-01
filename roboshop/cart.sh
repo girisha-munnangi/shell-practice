3b2f20642428494cb1c6986a16952be6dacadc8d
Girisha <girishamunnangi@gmail.com>
2026-02-01 23:29:38 +0530

    roboshop project

--- roboshop/cart.sh
+++ roboshop/cart.sh
@@ -52,7 +52,7 @@ cd /app
 npm install &>>$LOGS_FILE
 VALIDATE $? "installing npm"
 
-cp $SCRIPT_DIR/cart.service /etc/system/systemd/cart.service
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
 VALIDATE $? "copying cart service file"
 systemctl daemon-reload &>>$LOGS_FILE
 VALIDATE $? "deamon reload"
