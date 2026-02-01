ceda620d0195a3db45f14c00ef2a02f70f34315f
Girisha <girishamunnangi@gmail.com>
2026-02-01 22:20:06 +0530

    roboshop project

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -24,17 +24,14 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y &>>$LOGS_FILE
-VALIDATE $? "disabling redis"
-dnf module enable redis:7 -y &>>$LOGS_FILE
-VALIDATE $? "enabling redis"
-dnf install redis -y &>>$LOGS_FILE
-VALIDATE $? "installing redis"
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-systemctl enable redis &>>$LOGS_FILE
-VALIDATE $? "enabling redis"
-systemctl start redis &>>$LOGS_FILE
-VALIDATE $? "restating redis"
+dnf install mysql-server -y &>>$LOGS_FILE
+VALIDATE $? "INSTALLING MYSQL"
+
+systemctl enable mysqld &>>$LOGS_FILE
+systemctl start mysqld  
+VALIDATE $? "enabling and starting  MYSQL service"
+
+
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME - $START_TIME))
