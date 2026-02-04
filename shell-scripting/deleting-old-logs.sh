486923d86c10ea08562e7cf06d092420287da9c7
Girisha <girishamunnangi@gmail.com>
2026-02-04 23:28:43 +0530

    delete logs

--- shell-scripting/deleting-old-logs.sh
+++ shell-scripting/deleting-old-logs.sh
@@ -5,7 +5,7 @@ if [ ! -d $LOGS_DIR ]; then
     echo "$LOGS_DIR does not exists"
     exit 1
 fi
-FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
+FILES_TO_DELETE=$( find $LOGS_DIR -name "*.log" -mtime +14 )
 
 while IFS= read -r filepath; do
     echo "deleting file: $filepath"
